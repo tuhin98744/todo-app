599f21a6b8bdbcd85d6a5005983860cc2633be13
tuhin <tuhin.nnv@gmail.com>
2023-10-18 11:35:54 +0530

    V1 Starting Project

---
+++ main.py
@@ -0,0 +1,14 @@
+#userinput
+user_prompt = "Enter todo: "
+
+#store input values in todos1,2,3
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+
+#todos contain todo1,2,3
+todos = [todo1, todo2,  todo3]
+
+#Print todos
+print("your saved todos is ", todos)
